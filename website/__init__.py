4d1bbf76bdd4d420a3657063f2744f88862bd9fb
eviepk12 <eviepk12@gmail.com>
2022-09-06 20:39:23 +0700

    added progress

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemyv as db
+
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'jaisdfaadfhasdfhsdf' # secret key that should not be known other than the developers for decrypting data
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}" # locates the sql alchemy database
+    db.init_app()
 
     from .views import views
     from .auth import auth
